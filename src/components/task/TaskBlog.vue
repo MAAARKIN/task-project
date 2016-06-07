0a59f5db66abbf78959a8db077973fc0ce82c3a4
Marcos Filho <m.filhow@gmail.com>
2016-06-06 23:26:17 -0300

    integracao com jquery e wordpress funcionando.

--- src/components/task/TaskBlog.vue
+++ src/components/task/TaskBlog.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="panel panel-default">
 		<div class="panel-heading">Task Blog</div>
-		<div class="panel-body">
-			<div v-for="post in posts">
-				{{post.content | html}}
-			</div >
+		<div class="panel-body" id="teste">
+			<!-- <div v-for="post in posts" id="posts">
+				{{post.content | html 'posts'}}
+			</div > -->
 		</div>
 	</div>
 </template>
@@ -17,19 +17,40 @@
 			}
 		},
 
+		created() {
+			console.log('created')
+			this.$http.get('https://public-api.wordpress.com/rest/v1.1/sites/publicvoidfilhow.wordpress.com/posts')
+			.then(function (response) {
+				this.posts = response.data.posts;
+				console.log(response.data.posts);
+				console.log('recebeu');
+				this.$dispatch('updatePosts');
+			}, function (error) {
+				console.log('error');
+				console.log(error);
+			});
+		},
+
 		ready() {
-			// this.$http.get('https://public-api.wordpress.com/rest/v1.1/sites/publicvoidfilhow.wordpress.com/posts')
-			// .then(function (response) {
-			// 	// this.posts = $.parseHTML(response.data.posts);
-			// 	this.posts = response.data.posts
-			// 	console.log(response.data.posts);
-			// }, function (error) {
-			// 	console.log(error);
-			// });
-			let post = {
-				content: '<h1>Teste</h1><br>mensagem'
-			};
-			this.posts.push(post);
+			console.log('ready')
+			// console.log(this.posts.length);
+			// // console.log(this.posts[0].content);
+			// this.posts.forEach(function (element) {
+			// 	console.log("percorrendo");
+			// 	console.log(element.id);
+			// })
+			// console.log('terminou');
+		},
+
+		events: {
+			'updatePosts': function() {
+				console.log("new posts")
+				console.log(this.posts.length)
+				this.posts.forEach(function (post) {
+					console.log('post id'+post.ID);
+					$('#teste').append(post.content);
+				})
+			}
 		}
 	}
 </script>
--- src/main.js
+++ src/main.js
@@ -4,14 +4,6 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-// Vue.http.options.root = 'https://public-api.wordpress.com/rest/v1.1/sites/publicvoidfilhow.wordpress.com/';
-// console.log(Vue.http.options)
-/* eslint-disable no-new */
-Vue.filter('html', function (value) {
-	console.log(value)
-  	return $.parseHTML(value);
-});
-
 new Vue({
   el: 'body',
   components: { App }
